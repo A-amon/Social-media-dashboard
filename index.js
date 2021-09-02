0f9d029a18ac4d5186c414b27e2f3e13790646a1
A-amon <64846892+A-amon@users.noreply.github.com>
2021-09-02 09:18:28 +0800

    Update toggle button to radio buttons
    Replace button element for toggle with radio buttons and fieldset

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 
-const switchLabel = document.querySelector('.header__mode-label')
-const headerSwitch = document.querySelector('.header__mode-switch')
+/**
+ * Toggle using button
+ */
+
+// const switchLabel = document.querySelector('.header__mode-label')
+// const headerSwitch = document.querySelector('.header__mode-switch')
+
+// /**
+//  * Toggle light/ dark mode
+//  */
+// const toggleSwitch = () => {
+//     const checked = headerSwitch.getAttribute('aria-checked') === 'true'
+
+//     headerSwitch.setAttribute('aria-checked', !checked)
+//     switchLabel.textContent = `${!checked ? 'Dark' : 'Light'} Mode`
+
+//     document.body.classList.toggle('dark')
+
+//     headerSwitch.focus()
+// }
+
+// headerSwitch.addEventListener('click', toggleSwitch)
+
+
 
 /**
- * Toggle light/ dark mode
+ * Toggle using radio buttons
  */
+const switchRadios = document.querySelectorAll('.header__switch-toggle')
+
 const toggleSwitch = () => {
-    const checked = headerSwitch.getAttribute('aria-checked') === 'true'
-
-    headerSwitch.setAttribute('aria-checked', !checked)
-    switchLabel.textContent = `${!checked ? 'Dark' : 'Light'} Mode`
-
     document.body.classList.toggle('dark')
-
-    headerSwitch.focus()
 }
 
-headerSwitch.addEventListener('click', toggleSwitch)
+for (let radio of switchRadios) {
+    radio.addEventListener('change', toggleSwitch)
+}
